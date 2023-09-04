b4f85493a16de5de866795d26041c3fe69bf41d6
Julian <julianser@hotmail.com.ar>
2023-09-04 19:43:13 -0300

    Se agrego una fuente al navbar y se realizaron los navlinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
-import {Container} from "react-bootstrap"
 import Menu from './components/common/Menu'
 function App() {
 
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -4,12 +4,15 @@ const Menu = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand className="fs-1 logo" href="#home">W.</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">New</Nav.Link>
+            <Nav.Link href="#link">Popular</Nav.Link>
+            <Nav.Link href="#link">Trending</Nav.Link>
+            <Nav.Link href="#link">Categories</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
