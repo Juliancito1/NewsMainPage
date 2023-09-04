78bdb446f48699d5fa96c6500d912da47bee5775
Julian <julianser@hotmail.com.ar>
2023-09-04 19:31:39 -0300

    Se realizo la base del navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
+import {Container} from "react-bootstrap"
+import Menu from './components/common/Menu'
 function App() {
 
   return (
-    <>
-     
-    </>
+    <section>
+        <Menu></Menu>
+    </section>
   )
 }
 
