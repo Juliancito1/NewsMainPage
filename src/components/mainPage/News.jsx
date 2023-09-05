4cd8047a0f2f371596d9b71d3d549c4e7242b98f
Julian <julianser@hotmail.com.ar>
2023-09-05 00:05:47 -0300

    Se agrego una fuente a la pagina y se modificaron clases en la seccion noticias

--- src/components/mainPage/News.jsx
+++ src/components/mainPage/News.jsx
@@ -5,27 +5,27 @@ const News = () => {
         <Container className="border news p-3">
             <h2 className="text-warning mt-4 display-6 fw-bold">New</h2>
             <article className="text-light my-4">
-                <a href="" className="text-decoration-none text-light"><h4 className="news-side">
+                <a href="" className="text-decoration-none text-light"><h4 className="news-side fw-bold">
                     Hydrogen VS Electric Cars
                     </h4>
                     </a>
-                <p className="lead">Will hydrogen-fueled cars ever catch up to Evs?</p>
+                <p className="lead text-secondary">Will hydrogen-fueled cars ever catch up to Evs?</p>
                 <hr></hr>
             </article>
             <article className="text-light my-4">
-                <a href="" className="text-decoration-none text-light"><h4 className="news-side">
+                <a href="" className="text-decoration-none text-light"><h4 className="news-side fw-bold">
                     The Downsides of AI Artistry
                     </h4>
                     </a>
-                <p className="lead">What are the possible adverse effects of on-demand AI image generation?</p>
+                <p className="lead text-secondary">What are the possible adverse effects of on-demand AI image generation?</p>
                 <hr></hr>
             </article>
             <article className="text-light my-4">
-                <a href="" className="text-decoration-none text-light"><h4 className="news-side">
+                <a href="" className="text-decoration-none text-light"><h4 className="news-side fw-bold">
                     Is VC Funding Drying Up?
                     </h4>
                     </a>
-                <p className="lead fs-5">Private funding by VC firms is down 50% YOY. We take a look at what that means</p>
+                <p className="text-secondary lead">Private funding by VC firms is down 50% YOY. We take a look at what that means</p>
             </article>
         </Container>
     );
